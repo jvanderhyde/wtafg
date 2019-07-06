24414b444a1469da2f5f772c06e0a7168e64663c
James Vanderhyde <vanderhyde@sxu.edu>
2019-07-05 22:04:15 -0400

    Launch a window

--- WtfgJavaFX/src/us/vanderhyde/wtfg/WtfgJavaFX.java
+++ WtfgJavaFX/src/us/vanderhyde/wtfg/WtfgJavaFX.java
@@ -1,56 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//World's tiniest fighting game JavaFX application
+//Created by James Vanderhyde, 5 July 2019
+
 package us.vanderhyde.wtfg;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 
-/**
- *
- * @author jv4
- */
 public class WtfgJavaFX extends Application
 {
-    
     @Override
     public void start(Stage primaryStage)
     {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>()
-        {
-            
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Hello World!");
-            }
-        });
+        Canvas canvas = new Canvas(1280, 720);
+        Group root = new Group();
+        root.getChildren().add( canvas );
+        Scene scene = new Scene(root);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("World's Tiniest Fighting Game");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         launch(args);
     }
-    
 }
