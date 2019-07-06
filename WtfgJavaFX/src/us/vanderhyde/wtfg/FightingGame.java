6519f0df95a3633e413a2261f298a67337e90531
James Vanderhyde <vanderhyde@sxu.edu>
2019-07-05 23:51:23 -0400

    User control

--- WtfgJavaFX/src/us/vanderhyde/wtfg/FightingGame.java
+++ WtfgJavaFX/src/us/vanderhyde/wtfg/FightingGame.java
@@ -12,8 +12,7 @@ import us.vanderhyde.ecs.Game;
 public class FightingGame
 {
     public static enum Component {
-        hitbox,
-        name
+        hitbox, name, playerControl
     };
     private final Game<Component> game = new Game<>();
     
@@ -23,17 +22,27 @@ public class FightingGame
         game.addEntity(player);
         game.addComponent(player, Component.name, "Player");
         game.addComponent(player, Component.hitbox, new Rectangle(10,600,20,50));
+        game.addComponent(player, Component.playerControl, new Object());
     }
     
     public void update(long delta, Collection<String> input, GraphicsContext gc)
     {
         //Update physics
-        System.out.println(game);
+        for (Entity e:game.getEntities(Component.playerControl))
+        {
+            Rectangle r = (Rectangle)game.getComponent(e, Component.hitbox);
+            if (input.contains("LEFT"))
+                r.setX(r.getX()-2);
+            if (input.contains("RIGHT"))
+                r.setX(r.getX()+2);
+        }        
+        
+        //Blank screen
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
         
         //Draw everything
         for (Entity e:game.getEntities(Component.hitbox))
         {
-            System.out.println("update");
             Rectangle r = (Rectangle)game.getComponent(e, Component.hitbox);
             gc.strokeRect(r.getX(), r.getY(), r.getWidth(), r.getHeight());
         }
--- WtfgJavaFX/src/us/vanderhyde/wtfg/WtfgJavaFX.java
+++ WtfgJavaFX/src/us/vanderhyde/wtfg/WtfgJavaFX.java
@@ -3,11 +3,13 @@
 
 package us.vanderhyde.wtfg;
 
+import java.util.HashSet;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class WtfgJavaFX extends Application
@@ -20,6 +22,12 @@ public class WtfgJavaFX extends Application
         root.getChildren().add( canvas );
         final Scene scene = new Scene(root);
         
+        HashSet<String> input = new HashSet<>();
+        scene.setOnKeyPressed((KeyEvent e) ->
+            input.add(e.getCode().toString()));
+        scene.setOnKeyReleased((KeyEvent e) ->
+            input.remove(e.getCode().toString()));
+        
         final FightingGame game = new FightingGame();
         
         new AnimationTimer()
@@ -27,7 +35,7 @@ public class WtfgJavaFX extends Application
             @Override
             public void handle(long currentNanoTime)
             {
-                game.update(currentNanoTime, null, canvas.getGraphicsContext2D());
+                game.update(currentNanoTime, input, canvas.getGraphicsContext2D());
             }
         }.start();
         
