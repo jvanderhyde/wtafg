58a2c1486ade5fe8ffde46d630e9a6ed9471b3ac
James Vanderhyde <vanderhyde@sxu.edu>
2021-05-19 16:45:43 -0500

    Removed the AI for now

--- WtfgJavaFX/src/us/vanderhyde/wtfg/FightingGame.java
+++ WtfgJavaFX/src/us/vanderhyde/wtfg/FightingGame.java
@@ -42,13 +42,13 @@ public class FightingGame
         game.add(player2, new CombatPoseComponent(CombatSystem.Pose.block));
         game.add(player2, new FacingDirection(MovementSystem.Facing.left));
 
-        Entity dummy = new Entity();
+        /*Entity dummy = new Entity();
         game.addEntity(dummy);
         game.add(dummy, "Dummy player");
         game.add(dummy, new FighterPosition(450));
         game.add(dummy, new AIControlComponent());
         game.add(dummy, new CombatPoseComponent(CombatSystem.Pose.block));
-        game.add(dummy, new FacingDirection(MovementSystem.Facing.left));
+        game.add(dummy, new FacingDirection(MovementSystem.Facing.left));*/
         
         System.out.println(game);
     }
